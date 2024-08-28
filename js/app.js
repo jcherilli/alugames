b9fd53843ebcb70279daab1143d75eccd7c86ef1
JulioSantos <jcherilli@gmail.com>
2024-08-28 15:08:58 -0300

    finalizando desafio alugames e ordenacao de numeros

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+let quantidadeAlugados = 0;
+
+function exibirQuantidade() {
+    console.log(`Total de jogos alugados: ${quantidadeAlugados}`);
+}
+
 function alterarStatus(id) {
     let gameSelecionado = document.getElementById(`game-${id}`);
     let imagemSelecionado = gameSelecionado.querySelector('.dashboard__item__img');
@@ -7,12 +13,32 @@ function alterarStatus(id) {
     // alert('Nome do jogo' + ` - ${nomeJogoSelecionado.textContent}`);
 
     if (imagemSelecionado.classList.contains('dashboard__item__img--rented')) {
-        imagemSelecionado.classList.remove('dashboard__item__img--rented');
-        botaoSelecionado.classList.remove('dashboard__item__button--return');
-        botaoSelecionado.textContent = 'Alugar';
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogoSelecionado.textContent}?`)) {
+            imagemSelecionado.classList.remove('dashboard__item__img--rented');
+            botaoSelecionado.classList.remove('dashboard__item__button--return');
+            botaoSelecionado.textContent = 'Alugar';
+        }
+        quantidadeAlugados--;
     } else {
         imagemSelecionado.classList.add('dashboard__item__img--rented');
         botaoSelecionado.classList.add('dashboard__item__button--return');
         botaoSelecionado.innerHTML = 'Devolver';
+        quantidadeAlugados++;
     }
+
+    exibirQuantidade()
+    ordenarNumeros(6, 5, 1, 2, 3, 4);
+
+}
+
+document.addEventListener('DOMContentLoaded',
+    function() {
+        quantidadeAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+        exibirQuantidade();
+});
+
+function ordenarNumeros(a, b, c, d, e, f) {
+    console.log(`Números antes de ordenar: ${numerosOrdenados.join(', ')}`);
+    const numerosOrdenados = [a, b, c, d, e, f].sort((x, y) => x - y);
+    console.log(`Números ordenados: ${numerosOrdenados.join(', ')}`);
 }
