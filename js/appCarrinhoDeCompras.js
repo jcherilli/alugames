f9d84f0443263aeeb363ad106766d666b57821e3
JulioSantos <jcherilli@gmail.com>
2024-08-28 23:45:21 -0300

    retirado alerts do appCarrinhoDeCompras

--- js/appCarrinhoDeCompras.js
+++ js/appCarrinhoDeCompras.js
@@ -6,21 +6,14 @@ function adicionar() {
     let carrinho = document.getElementById('lista-produtos');
     let totalCompra = parseFloat(document.getElementById('valor-total').innerText.split('R$')[1]);
     
-    // alert(nome);
-    // alert(preco);
-    // alert(quantidade.value);
     let totalProduto = quantidade * preco;
-    // alert(totalProduto);
 
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}x</span> ${nome} <span class="texto-azul">R$${totalProduto}</span>
         </section>`;
     document.getElementById('quantidade').value = '';
 
-    alert(totalCompra);
-
     totalCompra = totalCompra + parseFloat(totalProduto);
-    alert(totalCompra);
     document.getElementById('valor-total').innerText = `R$ ${totalCompra}`;
 }
 
